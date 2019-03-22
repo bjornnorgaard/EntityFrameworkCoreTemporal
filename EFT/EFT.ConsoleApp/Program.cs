9c7d9cdbc5f5e61ce9b35526f43ceb9aeccc11af
Bjørn Nørgaard <bjors@eniig.dk>
2019-03-22 15:42:56 +0100

    Improved program.cs with seed and some console logging for transparency

--- EFT/EFT.ConsoleApp/Program.cs
+++ EFT/EFT.ConsoleApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using EFT.Domain;
 using EFT.Repository;
 using EFT.Repository.Extensions;
@@ -12,9 +13,13 @@ namespace EFT.ConsoleApp
     {
         static void Main(string[] args)
         {
+            Console.Write("Clearing and creating database... ");
             var db = new EftContext();
+            db.Database.EnsureDeleted();
             db.Database.Migrate();
+            Console.WriteLine("Done");
 
+            Console.Write("Seeding database... ");
             var students = new List<Student>
             {
                 new Student{Name = "John Doe"},
@@ -22,27 +27,31 @@ namespace EFT.ConsoleApp
                 new Student{Name = "Kasper Skye"},
             };
 
-            if (!db.Students.Any())
-            {
-                db.Students.AddRange(students);
-                db.SaveChanges();
-            }
+            db.Students.AddRange(students);
+            db.SaveChanges();
+            Console.WriteLine("Done");
 
+            Console.Write("Changing first student... ");
             var student = db.Students.FirstOrDefault();
             student.Name = "Mister Sir";
             db.SaveChanges();
+            Console.WriteLine("Done");
 
+            Console.Write("Fetching revisions of first student... ");
             var allRevisions = db.Students.AsNoTracking()
                 .Between(DateTime.MinValue, DateTime.Now)
                 .Where(s => s.Id == 1)
                 .ToList();
+            Console.WriteLine("Done");
 
+            Console.WriteLine("");
             foreach (var r in allRevisions)
             {
                 Console.WriteLine($"Id: {r.Id}, Name: {r.Name}");
             }
 
-            var wait = Console.ReadKey();
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadKey();
         }
     }
 }
