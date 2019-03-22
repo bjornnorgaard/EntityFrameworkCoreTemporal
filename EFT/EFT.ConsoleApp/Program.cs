7dbb3fc61d0354c9e687423075835b2fc693a85f
Bjørn Nørgaard <bjors@eniig.dk>
2019-03-22 15:27:41 +0100

    Updated program.cs with some basic test

--- EFT/EFT.ConsoleApp/Program.cs
+++ EFT/EFT.ConsoleApp/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using EFT.Domain;
 using EFT.Repository;
 using EFT.Repository.Extensions;
 using Microsoft.EntityFrameworkCore;
@@ -13,10 +15,34 @@ namespace EFT.ConsoleApp
             var db = new EftContext();
             db.Database.Migrate();
 
+            var students = new List<Student>
+            {
+                new Student{Name = "John Doe"},
+                new Student{Name = "Simon Says"},
+                new Student{Name = "Kasper Skye"},
+            };
+
+            if (!db.Students.Any())
+            {
+                db.Students.AddRange();
+                db.SaveChanges();
+            }
+
+            var student = db.Students.FirstOrDefault();
+            student.Name = "Mister Sir";
+            db.SaveChanges();
+
             var allRevisions = db.Students.AsNoTracking()
                 .Between(DateTime.MinValue, DateTime.Now)
                 .Where(s => s.Id == 1)
                 .ToList();
+
+            foreach (var r in allRevisions)
+            {
+                Console.WriteLine($"Id: {r.Id}, Name: {r.Name}");
+            }
+
+            var wait = Console.ReadKey();
         }
     }
 }
